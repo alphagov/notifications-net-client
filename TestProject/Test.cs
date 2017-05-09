4e4030849dd48bfe07d53d069ffba3a804e4864a
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-05-09 14:25:59 +0100

    Trying to get NUnit to work

---
+++ TestProject/Test.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+namespace TestProject
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+			Assert.IsTrue(false);
+		}
+
+		[Test()]
+		public void TestCase1()
+		{
+			Assert.IsTrue(false);
+		}
+
+		[Test, Category("Integration")]
+		public void SendSmsTestWithPersonalisation()
+		{
+			Assert.IsTrue(false);
+		}
+	}
+}
