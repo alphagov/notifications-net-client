b43c54bf784d98a1b2d27ca8c960c88cb17a0efb
Imdad Ahad <imdad.ahad@digital.cabinet-office.gov.uk>
2017-09-22 15:51:52 +0100

    Remove build scripts that are no longer used

--- build/deploy.ps1
+++
@@ -1,6 +0,0 @@
-. .\get_project_version.ps1
-
-$currentVersion = Get-ProjectVersion -ProjectName "Notify"
-
-# Assumes you have added the Nuget Bintray source
-nuget push "Notify.$currentVersion.nupkg" -Source Bintray
