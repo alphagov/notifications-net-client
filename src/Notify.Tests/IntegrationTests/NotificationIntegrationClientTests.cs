1c59d592da372305361a469e8096f2db0caf6188
Alexey Bezhan <al@fueledbylemons.com>
2018-09-13 16:07:56 +0100

    Add integration test for email with document upload

--- src/Notify.Tests/IntegrationTests/NotificationIntegrationClientTests.cs
+++ src/Notify.Tests/IntegrationTests/NotificationIntegrationClientTests.cs
@@ -194,6 +194,36 @@ namespace Notify.Tests.IntegrationTests
 		}
 
 		[Test, Category("Integration")]
+		public void SendEmailWithDocumentPersonalisationTest()
+		{
+			byte[] pdfContents;
+
+			try
+			{
+				pdfContents = File.ReadAllBytes("../../../IntegrationTests/test_files/one_page_pdf.pdf");
+			}
+			catch (DirectoryNotFoundException)
+			{
+				pdfContents = File.ReadAllBytes("IntegrationTests/test_files/one_page_pdf.pdf");
+			}
+
+			Dictionary<String, dynamic> personalisation = new Dictionary<String, dynamic>
+			{
+				{ "name", NotificationClient.PrepareUpload(pdfContents) }
+			};
+
+			EmailNotificationResponse response =
+				this.client.SendEmail(FUNCTIONAL_TEST_EMAIL, EMAIL_TEMPLATE_ID, personalisation);
+
+			Assert.IsNotNull(response.id);
+			Assert.IsNotNull(response.template.id);
+			Assert.IsNotNull(response.template.uri);
+			Assert.IsNotNull(response.template.version);
+			Assert.AreEqual(response.content.subject, TEST_EMAIL_SUBJECT);
+			Assert.IsTrue(response.content.body.Contains("https://documents."));
+		}
+
+		[Test, Category("Integration")]
 		public void GetAllNotifications()
 		{
 			NotificationList notificationsResponse = this.client.GetNotifications();
