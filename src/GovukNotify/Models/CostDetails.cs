6b58342814e1bb3ae208eaf3d5c9e419c7b35eae
joybytes <171517790+joybytes@users.noreply.github.com>
2024-07-31 18:26:07 +0100

    chore: update docs to include new notification fields (cost_in_pounds, is_data_ready and cost_details)

---
+++ src/GovukNotify/Models/CostDetails.cs
@@ -0,0 +1,40 @@
+using Newtonsoft.Json;
+
+namespace Notify.Models
+{
+    public class CostDetails
+    {
+        [JsonProperty("billable_sms_fragments")]
+        public int? billableSmsFragments;
+
+        [JsonProperty("international_rate_multiplier")]
+        public double? internationalRateMultiplier;
+
+        [JsonProperty("sms_rate")]
+        public double? smsRate;
+
+        [JsonProperty("billable_sheets_of_paper")]
+        public int? billableSheetsOfPaper;
+
+        public string? postage;
+
+        public override bool Equals(object costDetail)
+        {
+            if (!(costDetail is CostDetails cd))
+            {
+                return false;
+            }
+
+            return billableSmsFragments == cd.billableSmsFragments &&
+                   internationalRateMultiplier == cd.internationalRateMultiplier &&
+                   smsRate == cd.smsRate &&
+                   billableSheetsOfPaper == cd.billableSheetsOfPaper &&
+                   postage == cd.postage;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
